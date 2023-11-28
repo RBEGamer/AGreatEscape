3be4d74158e953b6df6638586b8f0698c43f9b6e
Hack4Pott Team 10 <marcel.ochsendorf@gmail.com>
2023-11-28 22:00:32 +0100

    .

--- src/backend/backend/DBModelUser.py
+++ src/backend/backend/DBModelUser.py
@@ -4,7 +4,14 @@ class DBModelUser(object):
     climbrange: int = 5
     widthrange: int = 5
     operator: bool = False
+
+
+
+    # ALGORITHM USED VARS
     exit_reached: bool = False
+    current_postion_on_map_x: int = -1
+    current_postion_on_map_y: int = -1
+    target_exit: int = -1
 
 
     def __init__(self):
@@ -18,7 +25,10 @@ class DBModelUser(object):
             'climbrange': self.climbrange,
             'widthrange': self.widthrange,
             'operator': self.operator,
-            'exit_reached': self.exit_reached
+            'exit_reached': self.exit_reached,
+            'current_postion_on_map_x': self.current_postion_on_map_x,
+            'current_postion_on_map_y': self.current_postion_on_map_y,
+            'target_exit': self.target_exit
         }
 
     def __dict__(self):

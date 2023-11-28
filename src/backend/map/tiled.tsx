1d0200995209a748b06858ce83079a187c425880
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2023-11-28 22:57:37 +0100

    tilemap updated

---
+++ src/backend/map/tiled.tsx
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tiled" tilewidth="180" tileheight="380" tilecount="5" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <properties>
+   <property name="type" value="blocked"/>
+  </properties>
+  <image width="1" height="1" source="path_blocked.bmp"/>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="type" value="free"/>
+  </properties>
+  <image width="1" height="1" source="path_clear.bmp"/>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="type" value="exit_type_1"/>
+  </properties>
+  <image width="1" height="1" source="path_exit.bmp"/>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="type" value="exit_type_2"/>
+  </properties>
+  <image width="1" height="1" source="path_exit1.bmp"/>
+ </tile>
+ <tile id="4">
+  <image width="180" height="380" source="map_sized.bmp"/>
+ </tile>
+</tileset>

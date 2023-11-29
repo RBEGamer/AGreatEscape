a867073efacdd25237b4b50415acdabe7643d181
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2023-11-29 23:44:47 +0100

    2

--- src/backend/backend/ai.py
+++ src/backend/backend/ai.py
@@ -4,13 +4,13 @@ import numpy as np
 from numpy import genfromtxt
 import heapq
 
-
-#standard = 50
+# standard = 50
 occupancy_level = [100, 100, 50, 30, 20, 10, 5, 1, 3, 4, 5, 1]
-#exit_positions_sample = [{'x': 186, 'y': 89, 'special': False}, {'x': 1, 'y': 100, 'special': False}, {'x': 137, 'y': 176, 'special': False}, {'x': 141, 'y': 108, 'special': False}, {'x': 179, 'y': 5, 'special': True}, {'x': 215, 'y': 5, 'special': True}, {'x': 236, 'y': 95, 'special': True}, {'x': 281, 'y': 171, 'special': True}, {'x': 301, 'y': 87, 'special': True}, {'x': 331, 'y': 5, 'special': True}, {'x': 351, 'y': 95, 'special': True}, {'x': 378, 'y': 89, 'special': True}]
-#people_sample = [{"username": "1", "walkfast": 5, "widthrange": 5, "current_position_on_map_x": 159, "current_position_on_map_y": 80},{"username": "2", "walkfast": 5, "widthrange": 5, "current_position_on_map_x": 159, "current_position_on_map_y": 80},{"username": "3", "walkfast": 5, "widthrange": 5, "current_position_on_map_x": 159, "current_position_on_map_y": 80}]
-#f = genfromtxt("../map/floorplan.csv", delimiter=',')
 
+
+# exit_positions_sample = [{'x': 186, 'y': 89, 'special': False}, {'x': 1, 'y': 100, 'special': False}, {'x': 137, 'y': 176, 'special': False}, {'x': 141, 'y': 108, 'special': False}, {'x': 179, 'y': 5, 'special': True}, {'x': 215, 'y': 5, 'special': True}, {'x': 236, 'y': 95, 'special': True}, {'x': 281, 'y': 171, 'special': True}, {'x': 301, 'y': 87, 'special': True}, {'x': 331, 'y': 5, 'special': True}, {'x': 351, 'y': 95, 'special': True}, {'x': 378, 'y': 89, 'special': True}]
+# people_sample = [{"username": "1", "walkfast": 5, "widthrange": 5, "current_position_on_map_x": 159, "current_position_on_map_y": 80},{"username": "2", "walkfast": 5, "widthrange": 5, "current_position_on_map_x": 159, "current_position_on_map_y": 80},{"username": "3", "walkfast": 5, "widthrange": 5, "current_position_on_map_x": 159, "current_position_on_map_y": 80}]
+# f = genfromtxt("../map/floorplan.csv", delimiter=',')
 
 
 def dijkstra(grid, start, target):
@@ -19,7 +19,7 @@ def dijkstra(grid, start, target):
     prev = {start: None}
     dist[start] = 0
     queue = [(0, start)]
-    
+
     while queue:
         d, current = heapq.heappop(queue)
         if current == target:
@@ -38,32 +38,32 @@ def dijkstra(grid, start, target):
 
     return []
 
-def each_user(_user_item: DBModelUser.DBModelUser, _list_exits, _map):
-  #print(_user_item)
-  start = (_user_item.current_position_on_map_x, _user_item.current_position_on_map_y)
-  index = 0
-  step = []
-  if _user_item["widthrange"] <= 5:
-    for i in _list_exits:
-      path = dijkstra(_map, start,(i["x"],i["y"]))
-      step.append(len(path))
-  else:
-    for i in _list_exits:
-      if i["special"] == True:
-        path = dijkstra(_map, start,(i["x"],i["y"]))
-        step.append(len(path))
-      else:
-        step.append(9999999)
-  return(step)
 
+def each_user(_user_item: DBModelUser.DBModelUser, _list_exits, _map):
+    # print(_user_item)
+    start = (_user_item.current_position_on_map_x, _user_item.current_position_on_map_y)
+    index = 0
+    step = []
+    if _user_item["widthrange"] <= 5:
+        for i in _list_exits:
+            path = dijkstra(_map, start, (i["x"], i["y"]))
+            step.append(len(path))
+    else:
+        for i in _list_exits:
+            if i["special"] == True:
+                path = dijkstra(_map, start, (i["x"], i["y"]))
+                step.append(len(path))
+            else:
+                step.append(9999999)
+    return (step)
 
 
 def compute_new_people_exit_target(_people: [DBModelUser.DBModelUser], _exit_positions, _map) -> [int]:
-  result = []
-  for user in _people:
-    steps = each_user(user, _exit_positions, _map)
-    index_min = np.argmin(steps)
-    result.append(_exit_positions[index_min])
-  return result
+    result = []
+    for user in _people:
+        steps = each_user(user, _exit_positions, _map)
+        index_min = np.argmin(steps)
+        result.append(_exit_positions[index_min])
+    return result
 
 
